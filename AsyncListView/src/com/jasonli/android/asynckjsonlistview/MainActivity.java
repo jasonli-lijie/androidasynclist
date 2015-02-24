ed4ed3f1ab2b596f7b0adb0ed1740a9c31e7d316
Jason Li <jasonli.lijie@gmail.com>
2015-02-24 14:10:17 +1100

    moved all strings from code to resource

--- AsyncListView/src/com/jasonli/android/asynckjsonlistview/MainActivity.java
+++ AsyncListView/src/com/jasonli/android/asynckjsonlistview/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
-			dialog.setMessage("Downloading feed...");
+			dialog.setMessage(getResources().getString(R.string.downloading)); 
 			dialog.show();
 		}
 
